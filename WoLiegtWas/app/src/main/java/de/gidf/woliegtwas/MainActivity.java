e496838f7d602843d88d93df1281ec1a7bd5d6e1
TuanDungLuu <luutuan_d@yahoo.de>
2017-12-31 19:11:54 +0100

    Modified MainActivity

--- WoLiegtWas/app/src/main/java/de/gidf/woliegtwas/MainActivity.java
+++ WoLiegtWas/app/src/main/java/de/gidf/woliegtwas/MainActivity.java
@@ -1,13 +1,65 @@
 package de.gidf.woliegtwas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1, btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void InitializeApp() {
+        btn1 = (Button) findViewById(R.id.btnPlay);
+        btn2 = (Button) findViewById(R.id.btnHelp);
+        btn3 = (Button) findViewById(R.id.btnHighScore);
+
+
+        //
+        // EVENT LISTENERS
+        //
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlayActivity();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startHelpActivity();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startHighScoreActivity();
+            }
+        });
+
+    }
+    private void startPlayActivity() {
+        startActivity(
+                new Intent(this, MapActivity.class));
+    }
+
+
+    private void startHelpActivity() {
+        startActivity(
+                new Intent(this, HelpActivity.class));
+    }
+
+
+    private void startHighScoreActivity() {
+        startActivity(
+                new Intent(this, Highscore_Activity.class));
     }
 }
