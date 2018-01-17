19f9dabfeff842a1901adab1561a7f5128227ecc
TuanDungLuu <luutuan_d@yahoo.de>
2018-01-18 00:36:52 +0100

    Add Background Music and Icon-Launcher

--- WoLiegtWas/app/src/main/java/de/gidf/woliegtwas/MainActivity.java
+++ WoLiegtWas/app/src/main/java/de/gidf/woliegtwas/MainActivity.java
@@ -1,20 +1,50 @@
 package de.gidf.woliegtwas;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn1, btn2, btn3;
+    MediaPlayer bkgrdmsc;
+    private int lastbkgdchecked = MainActivity.bkgdchecked;
+
+    public static int bkgdchecked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (lastbkgdchecked ==0) {
+            bkgrdmsc = MediaPlayer.create(MainActivity.this, R.raw.background_music);
+            bkgrdmsc.setLooping(true);
+            bkgrdmsc.start();
+        }
+
         InitializeApp();
+        bkgdMusicoff();
+    }
+
+    public void bkgdMusicoff(){
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isChecked()){
+                    bkgdchecked = 1;
+                    bkgrdmsc.pause();
+                }else{
+                    bkgdchecked = 0;
+                    bkgrdmsc.start();
+                }
+            }
+        });
     }
     private void InitializeApp() {
         btn1 = (Button) findViewById(R.id.btnPlay);
@@ -64,5 +94,14 @@ public class MainActivity extends AppCompatActivity {
     private void startHighScoreActivity() {
         startActivity(
                 new Intent(this, Highscore_Activity.class));
+    }
+    @Override
+    public void onBackPressed() {
+        if(lastbkgdchecked ==0) {
+            bkgrdmsc.release();
+        }
+        MainActivity.bkgdchecked = 0;
+        super.onBackPressed();
+        finish();
     }
 }
